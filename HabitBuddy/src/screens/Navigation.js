25e3de920aa4c129c8e444b9f8c1109cb2eeaf38
ematbtfw <105008544+ematbtfw@users.noreply.github.com>
2023-09-02 14:31:24 +0300

    tasks

---
+++ HabitBuddy/src/screens/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import RegistrationScreen from './src/screens/RegistrationScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import HabitTrackingScreen from './src/screens/HabitTrackingScreen';
+import GoalSettingScreen from './src/screens/GoalSettingScreen';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="HabitTracking" component={HabitTrackingScreen} />
+        <Stack.Screen name="GoalSetting" component={GoalSettingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
